fd6171490c1f35f31f15e3e090cd12757a6f8667
KETAM Djillali <ketam.djalal@gmail.com>
2017-10-04 23:21:07 +0200

    modification de la class Fraction

--- MavenExo/src/main/java/TDmaven/Fraction.java
+++ MavenExo/src/main/java/TDmaven/Fraction.java
@@ -4,31 +4,30 @@
  * @author (KETAM)
  * @version (04-10-2017)
  */
-public class Fraction2 
+public class Fraction 
 {
     // variables d'instance - remplacez l'exemple qui suit par le vôtre
     private int numerateur;
     private int denominateur;
     
-    static Fraction2 ZERO = new Fraction2(0,1);
-    static Fraction2 UN = new Fraction2(1,1);
+    static Fraction ZERO = new Fraction(0,1);
+    static Fraction UN = new Fraction(1,1);
 
-    public Fraction2(int num, int denom)
+    public Fraction(int num, int denom)
     {
         this.numerateur = num;
         this.denominateur = denom;
     }
     
-    public Fraction2(int num)
+    public Fraction(int num)
     {
         this(num, 1);
     }
     
-    public Fraction2()
+    public Fraction()
     {
         this(0, 1);
     }
-    
     public int getNum(){ return numerateur; }
     public int getDenom(){ return denominateur; }
     
@@ -36,64 +35,9 @@ public class Fraction2
     {
         return ((double) numerateur / (double) denominateur); 
     }
-    
-    public Fraction2 add(Fraction2 f)
-    {
-        Fraction2 fraction = new Fraction2(numerateur * f.getDenom() + denominateur * f.getNum(), 
-                                    denominateur * f.getDenom());
-        return fraction;
-    }
-    
-    public int PGCD(int a,int b)
-    {
-        int x,y,z;
-        if (a > b) 
-        {   
-            x = a; 
-            y = b; 
-        } 
-        else 
-        {   x=b; 
-            y=a; 
-        } 
-        z= x % y; 
-       while(z!=0) 
-       { x = y; 
-         y = z; 
-         z = x % y; 
-       }
-       return y;
-    }
-    public Fraction2 reduction(Fraction2 f)
-    {
-        int p= PGCD ( f.numerateur , f.denominateur);
-        Fraction2 fraction = new Fraction2(f.getNum()/p,f.getDenom()/p);
-        return fraction;
-    }
-    
-    public boolean equal(Fraction2 f1,Fraction2 f2)
-    {
-        f1 = reduction(f1);
-        f2 = reduction(f2);
-        if((f1.numerateur == f2.numerateur)&&(f1.denominateur == f2.denominateur))
-            return true;
-        return false;
-    }
-    
     public String toString()
     {
         return (this.numerateur + "/" + this.denominateur);
-    }
-    
-
-    public boolean compare(Fraction2 f1,Fraction2 f2)
-    {
-        double x,y;
-        x= (double)f1.numerateur/(double) f1.denominateur;
-        y= (double) f2.numerateur /(double) f2.denominateur;
-        if(x>y)
-            return false;
-        return true;
     }
     
 
